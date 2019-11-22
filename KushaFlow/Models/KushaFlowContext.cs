73c35d74ce28d8f1dbc8316df295181e88cdd2e9
eseptehelp <eseptehelp@gmail.com>
2019-11-22 23:07:20 +0600

    Добавьте файлы проекта.

---
+++ KushaFlow/Models/KushaFlowContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace KushaFlow.Models
+{
+    public class KushaFlowContext : DbContext
+    {
+
+        public DbSet<Student> Students { get; set; }
+
+        public KushaFlowContext(DbContextOptions<KushaFlowContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
